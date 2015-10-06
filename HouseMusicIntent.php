5a6abd831cb207e60ae5f2eceb43ccab8a3bf9c5
Omni Adams <omni@digitaldarkness.com>
2015-10-05 20:19:00 -0500

    Removed the SSH stuff from the HouseMusicIntent

--- HouseMusicIntent.php
+++ HouseMusicIntent.php
@@ -27,43 +27,14 @@ class HouseMusicIntent implements Intent
     /**
      * Run the intent.
      *
-     * Verifies that the music filesystem is mounted, then starts the music.
      * @param \StdClass $slots Slots object
      * @throws \RuntimeException
      * @return string
      */
     public function run($slots = null)
     {
-        $this->mountFilesystem();
         exec('ssh ' . $this->ip . ' \'killall mplayer ; cd music ; mplayer -slave -input file=/home/pi/mplayer-control -shuffle -playlist party.m3u\'', $output);
 
         return 'If this house is a rockin, don\'t bother knockin!';
-    }
-
-    /**
-     * SSHes to the music machine and attempts to mount the filesystem if
-     * needed.
-     * @throws \RuntimeException
-     */
-    protected function mountFilesystem()
-    {
-        exec('ssh ' . $this->ip . ' ls music', $output);
-        if ([] !== $output) {
-            return;
-        }
-        error_log('Media player: SSHFS not mounted');
-
-        // SSH file system is unmounted for whatever reason...
-        exec('ssh ' . $this->ip . ' sshfs mordor-media:/home/music music');
-
-        // Test to see if it's connected now.
-        exec('ssh ' . $this->ip . ' ls music', $output);
-        if ([] === $output) {
-            // Still not connected, something's very wrong.
-            error_log('Failed to mount SSHFS on master pi');
-            throw new \RuntimeException(
-                'Master pie could not mount music directory. Please ask Omni to fix something.'
-            );
-        }
     }
 }
